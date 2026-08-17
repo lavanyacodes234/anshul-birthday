557886364f63236970e5eff5f134b34e35666115
lavanyacodes234 <lavanyakaushik2511@gmail.com>
2026-08-17 13:03:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function showLavuMessage() {
+
+    let message = document.getElementById("lavuMessage");
+
+    if (message.style.display === "block") {
+        message.style.display = "none";
+    } else {
+        message.style.display = "block";
+    }
+
+}
+function showKhushiMessage() {
+
+    let message = document.getElementById("khushiMessage");
+
+    if (message.style.display === "block") {
+        message.style.display = "none";
+    } else {
+        message.style.display = "block";
+    }
+
+}
